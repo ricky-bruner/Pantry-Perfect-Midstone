cad0259360b9d500299e263925d1ac1c2a18dfe7
ricky-bruner <alteredecho07@yahoo.com>
2018-09-12 13:10:11 -0500

    defaultvalue bug fixed for pantry item edit

---
+++ src/components/pantry/PantryItemEditCard.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+
+export default class PantryItemEditCard extends Component {
+    state = {
+        itemName: "",
+        itemAmount: "",
+        itemQuantityType: "",
+        renderEdit: false
+    }
+
+    componentDidMount(){
+        let pantryItem = {
+            itemName: this.props.pantryItem.name,
+            itemAmount: this.props.pantryItem.quantity,
+            itemQuantityType: this.props.quantityTypes.find(type => type.id === this.props.pantryItem.quantityTypeId).name
+        }
+        this.setState(pantryItem)
+    }
+
+    handleFieldChange = (evt) => {
+        const stateToChange = {}
+        stateToChange[evt.target.id] = evt.target.value
+        this.setState(stateToChange)
+    }
+
+    renderEdit = () => {
+        this.setState({renderEdit: true})
+    }
+    
+    render(){
+        return (
+            <div className="pantry-item-card">
+            {
+                !this.state.renderEdit &&
+                <div>
+                    <p>{this.props.pantryItem.name}</p>
+                    <p>{this.props.pantryItem.quantity} {this.props.quantityTypes.find(type => type.id === this.props.pantryItem.quantityTypeId).name.toLowerCase()}</p>
+                    <button onClick={this.renderEdit}>Edit</button>
+                    <button>Delete</button>
+                </div>
+            }
+            {
+                this.state.renderEdit &&
+                <div>
+                    <input type="text" id="itemName"  defaultValue={this.state.itemName} onChange={this.handleFieldChange} />
+                    <input type="text" id="itemAmount" defaultValue={this.state.itemAmount} onChange={this.handleFieldChange} />
+                    <select id="itemQuantityType" defaultValue={this.state.itemQuantityType} onChange={this.handleFieldChange}>
+                        {
+                            this.props.quantityTypes.map(type => {
+                                if(type.name !== this.state.itemQuantityType){
+                                    return <option key={type.id} defaultValue={type.name}>{type.name}</option>  
+                                } else {
+                                    return <option key={type.id} defaultValue={type.name}>{type.name}</option>
+                                }
+                            })
+                        }   
+                    </select>
+                    <button>Save Changes</button>
+                </div>
+            }
+            </div>
+        )
+    }
+}
--- src/components/pantry/PantryList.js
+++ src/components/pantry/PantryList.js
@@ -1,34 +1,45 @@
 import React, { Component } from "react";
 import PantryItemCard from "./PantryItemCard";
 import PantryItemAdd from "./PantryItemAdd";
+import PantryItemEditCard from "./PantryItemEditCard";
 
 export default class PantryList extends Component {
-    // state = {
-    //     pantryItems: [],
-    //     quantityTypes: []
-    // }
-    
-    // componentDidMount(){
-    //     console.log("CDM", this.props);
-    //     this.setState({
-    //         pantryItems: this.props.pantryItems,
-    //         quantityTypes: this.props.quantityTypes
-    //     })
-    // }
+    state = {
+        edit: false
+    }
+
+    renderEditCards = () => {
+        this.setState({edit: true})
+    }
+
+    renderCards = () => {
+        this.setState({edit: false})
+    }
 
     render(){
         console.log("render", this.props)
         return (
             <div className="pantry-list-container">
                 <h2>Pantry Items</h2>
+                <button onClick={this.renderEditCards}>Edit Pantry?</button>
                 <PantryItemAdd user={this.props.user}   
                                 addPantryItem={this.props.addPantryItem} 
                                 pantryItems={this.props.pantryItems} 
                                 quantityTypes={this.props.quantityTypes} />
                 {
+                    !this.state.edit &&
                     this.props.pantryItems.map(item => {
                         return <PantryItemCard key={item.id} pantryItem={item} quantityTypes={this.props.quantityTypes} />
                     })
+                }
+                {
+                    this.state.edit &&
+                    <React.Fragment>
+                        <button onClick={this.renderCards}>Finished Editing</button>
+                        {
+                            this.props.pantryItems.map(item => <PantryItemEditCard key={item.id} pantryItem={item} quantityTypes={this.props.quantityTypes} pantryItems={this.props.pantryItems} user={this.props.user} />)
+                        }
+                    </React.Fragment>
                 }
             </div>
         )
