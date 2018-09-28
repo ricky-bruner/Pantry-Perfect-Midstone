fcb113ad8f60148c70e4feab308b58d0bb0ea740
ricky-bruner <alteredecho07@yahoo.com>
2018-09-28 15:45:27 -0500

    added an styled labels as well as restructure

--- src/components/recipes/IngredientEditCard.js
+++ src/components/recipes/IngredientEditCard.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import DataManager from "../../modules/DataManager";
-import { Message, Button, Icon, Input, ButtonContent } from "semantic-ui-react"
+import { Message, Button, Icon, Input } from "semantic-ui-react"
 export default class IngredientEditCard extends Component {
     state = {
         editQuantity: false,
@@ -48,70 +48,70 @@ export default class IngredientEditCard extends Component {
         DataManager.delete("recipePantryItems", this.props.recipeItem.id)
         .then(() => this.props.updateRecipeItemState())
     }
-
+    
     render(){
         return (
-            <Message size="tiny" floating className="ingredient-card">
-                <div>
-                    <div>
-                        <p>{this.props.ingredient.name}</p>
+            <Message size="tiny" floating>
+                <div className="pre-edit-header">
+                    <div className="ingredient-edit-details">
+                        <p className="edit-ingredient-title">{this.props.ingredient.name}</p>
+                        {
+                            !this.state.editQuantity &&
+                            <p className="align-right edit-ingredient-quantity">{this.props.ingredient.quantity} {this.props.ingredient.type.toLowerCase()}</p>
+                        }
                     </div>
-                    {
-                        this.state.noChanges &&
-                        this.state.editQuantity &&
-                        <Message error size="mini">You didn't change anything</Message>
-                    }
-                    {
-                        this.state.editQuantity &&
-                        <div>
+                    <div className="button-right">
+                        {
+                            !this.state.editQuantity &&
                             <div>
-                                <Input type="number" size="mini" id="quantity" defaultValue={this.props.ingredient.quantity} onChange={this.handleFieldChange} />
-                                <Input list="types" size="mini" id="quantityType" defaultValue={this.props.ingredient.type} onChange={this.handleFieldChange} />
-                                    <datalist id="types">
-                                        {
-                                            this.props.quantityTypes.map(type => <option key={`${this.props.recipeItem.id}-${type.id}`} value={type.name} />)
-                                        }
-                                    </datalist>
+                                <Button size="mini" basic animated color="orange" onClick={this.editQuantity}>
+                                    <Button.Content visible>Edit</Button.Content>
+                                    <Button.Content hidden><Icon name="exchange" /></Button.Content>
+                                </Button>
                             </div>
-                            <Button size="mini" color="orange" animated onClick={this.updateQuantity}>
-                                    <Button.Content visible>Save</Button.Content>
-                                    <Button.Content hidden><Icon name="checkmark"/></Button.Content>
+                        }
+                        {
+                            this.state.editQuantity &&
+                            <div>
+                                <Button size="mini" animated basic color="orange" onClick={this.cancelQuantityEdit}>
+                                    <Button.Content visible>Cancel</Button.Content>
+                                    <Button.Content hidden><Icon name="ban" /></Button.Content>
+                                </Button>
+                            </div>
+                        }
+                        <div>
+                            <Button size="mini" basic animated color="red" onClick={this.removeIngredient}>
+                                <Button.Content visible>Remove</Button.Content>
+                                <Button.Content hidden><Icon name="remove circle" /></Button.Content>
                             </Button>
                         </div>
-                    }
-                    {
-                        !this.state.editQuantity &&
-                        <div>
-                            <p>{this.props.ingredient.quantity} {this.props.ingredient.type.toLowerCase()}</p>
-                        </div>
-                    }
-                </div>
-                <div className="button-right">
-                    {
-                        !this.state.editQuantity &&
-                        <div>
-                            <Button size="mini" basic animated color="orange" onClick={this.editQuantity}>
-                                <Button.Content visible>Edit</Button.Content>
-                                <Button.Content hidden><Icon name="exchange" /></Button.Content>
-                            </Button>
-                        </div>
-                    }
-                    {
-                        this.state.editQuantity &&
-                        <div>
-                            <Button size="mini" animated basic color="orange" onClick={this.cancelQuantityEdit}>
-                                <Button.Content visible>Cancel</Button.Content>
-                                <Button.Content hidden><Icon name="ban" /></Button.Content>
-                            </Button>
-                        </div>
-                    }
-                    <div>
-                        <Button size="mini" basic animated color="red" onClick={this.removeIngredient}>
-                            <Button.Content visible>Remove</Button.Content>
-                            <Button.Content hidden><Icon name="remove circle" /></Button.Content>
-                        </Button>
                     </div>
                 </div>
+                {
+                    this.state.editQuantity &&
+                    <div className="post-edit-footer">
+                        {
+                            this.state.noChanges &&
+                            this.state.editQuantity &&
+                            <Message error size="mini">You didn't change anything</Message>
+                        }
+                        <div>
+                            <Input type="number" size="mini" label={{content: "Amount", color: "orange"}} labelPosition="left" className="input-margin" id="quantity" defaultValue={this.props.ingredient.quantity} onChange={this.handleFieldChange} />
+                            <Input list="types" size="mini" label={{content: "Amount", color: "orange"}} labelPosition="left" className="input-margin" id="quantityType" defaultValue={this.props.ingredient.type} onChange={this.handleFieldChange} />
+                                <datalist id="types">
+                                    {
+                                        this.props.quantityTypes.map(type => <option key={`${this.props.recipeItem.id}-${type.id}`} value={type.name} />)
+                                    }
+                                </datalist>
+                        </div>
+                        <div className="button-center">
+                            <Button size="mini" color="orange" animated onClick={this.updateQuantity}>
+                                <Button.Content visible>Save</Button.Content>
+                                <Button.Content hidden><Icon name="checkmark"/></Button.Content>
+                            </Button>
+                        </div>
+                    </div>
+                }
             </Message>
         )
     }
